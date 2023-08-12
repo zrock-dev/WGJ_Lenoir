c1149733ca76336e6ecaf90ff87bfb0fd43b09a7
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2023-08-12 11:52:40 -0400

    jump movement

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
 @export var move_speed: int = 100
+@export var jump_speed: int = 200
+
+const gravity = 9.81
+ 
 
 func _physics_process(delta):
-	var direction = Vector2(
-		Input.get_action_strength("rigth") - Input.get_action_strength("left"), 0
-	)
+	var direction = Input.get_axis("left", "rigth")
+	velocity.x = move_speed * direction
 	
-	velocity = direction * move_speed
+	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
+		velocity.y -= jump_speed
+		
+	velocity.y += gravity
 	
 	move_and_slide()
+	
 
