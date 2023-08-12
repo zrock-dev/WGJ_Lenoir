56c2c6d3f6fac3b5b04cd0327cd0e986e61cd9b2
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2023-08-12 13:15:22 -0400

    now it works

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -4,14 +4,14 @@ class_name Player
 const MOVE_SPEED: int = 100
 const JUMP_SPEED: int = 200
 
-const GRAVITY: int = 9.81
+const GRAVITY: float = 9.81
  
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = Input.get_axis("left", "rigth")
 	velocity.x = MOVE_SPEED * direction
 	
-	if is_on_floor() and Input.is_action_just_pressed("ju"):
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y -= JUMP_SPEED
 		
 	velocity.y += GRAVITY
