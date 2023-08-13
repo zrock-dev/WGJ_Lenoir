4a2d976e765e47d3dd3aa8eb4da3523f274ccd58
Santiago C. Saavedra <santisaavcon+commits@gmail.com>
2023-08-13 13:09:40 -0400

    chore: update the first level pointer

--- scripts/main_menu/main_menu.gd
+++ scripts/main_menu/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/player/Player.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/esophagus/Level.tscn") # Replace with function body.
 
 func _on_button_2_pressed():
 	get_tree().quit()
