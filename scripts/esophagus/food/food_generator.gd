f3bc4af63b09233092d6c81c2f512e05f8234c59
Santiago C. Saavedra <santisaavcon+commits@gmail.com>
2023-08-13 12:28:19 -0400

    fix: food generation adjustment
    The food generation has been updated to reduce the generation boundaries
    of the right side of the screen. Also the spawn of objects as a bunch
    has been deleted and instead it will be one per timeout.

--- scripts/esophagus/food/food_generator.gd
+++ scripts/esophagus/food/food_generator.gd
@@ -2,7 +2,7 @@ extends Node
 class_name FoodGenerator
 
 const FOOD_SPAWN_TIMEOUT: float = 4.0
-const FOOD_SPAWN_AMOUNT: int = 2
+const FOOD_SPAWN_AMOUNT: int = 1
 
 const LEFT_X_BOUNDARY: int = 165
 const RIGHT_X_BOUNDARY: int = 960
@@ -35,11 +35,12 @@ func _ready():
 
 func on_spawn_food_piece() -> void:
 	var food: Food
-	for element in range(FOOD_SPAWN_AMOUNT-1):
-		food = FoodScene.instantiate()
-		food.create(generate_random_position())
-		add_child(food)
+	var index = randi_range(0, 2)
+	var object = food_objects[index]
+	food = object.instantiate()
+	food.create(generate_random_position())
+	add_child(food)
 
 func generate_random_position() -> Vector2:
-	var x_pos := randf_range(LEFT_X_BOUNDARY-5, RIGHT_X_BOUNDARY-5)
+	var x_pos := randf_range(LEFT_X_BOUNDARY-5, RIGHT_X_BOUNDARY-10)
 	return Vector2(x_pos, screen_height)
