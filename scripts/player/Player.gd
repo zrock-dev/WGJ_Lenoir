326e266c88945849325f8d3840755e9c21784edd
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2023-08-13 13:15:30 -0400

    aply sprites

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -8,9 +8,25 @@ const GRAVITY: float = 9.81
  
 
 func _physics_process(_delta):
-	var direction = Input.get_axis("left", "rigth")
+	var direction = Input.get_axis("left", "rigth")	
+		
 	velocity.x = MOVE_SPEED * direction
 	
+	$AnimatedSprite2D.flip_h = direction < 0
+	
+	if is_on_floor():
+		if velocity.x > 0:
+			$AnimatedSprite2D.play("walk")
+		else:
+			$AnimatedSprite2D.play("idle")
+			
+	else :
+		if velocity.y < 0:
+			$AnimatedSprite2D.play("jump")
+		else:
+			$AnimatedSprite2D.play("fall")
+		
+		
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y -= JUMP_SPEED
 		
@@ -18,4 +34,3 @@ func _physics_process(_delta):
 	
 	move_and_slide()
 	
-
